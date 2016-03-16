e5589d4b5fdf06aca30a1400fef6f242b33fe46c
eugene <compuniti@mail.ru>
2016-03-16 14:14:55 +0300

    тест

---
+++ EditableWidget.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author: Eugene
+ * @date: 16.03.16
+ * @time: 8:27
+ */
+
+namespace digitalmonk\widgets\EditableWidget;
+
+
+use yii\widgets\InputWidget;
+
+class EditableWidget extends InputWidget
+{
+    public function init()
+    {
+
+    }
+
+    public function run()
+    {
+        return $this->render('index');
+    }
+}
---
+++ views/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author: Eugene
+ * @date: 16.03.16
+ * @time: 14:13
+ */
+
+?>
+
+<div>Виджет</div>
